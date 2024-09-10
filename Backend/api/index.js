81e63ceb6d1535deb10d81c991ff49c155c2fd26
Srinivasa Rao Sadineni <sadinenisrinivasarao6@gmail.com>
2024-09-10 16:24:46 -0500

    vercel backend update

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,3 +1,29 @@
-import app from '../server.js';  // Import your app from the main Express setup
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import cartRouter from "./routes/CartRoute.js";
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: ["https://deploy-food-delivery-page.vercel.app"],
+    methods: ['POST', 'GET'],
+    credentials: true
+}));
+
+connectDB();
+
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.get("/", (req, res) => {
+    res.send("API working");
+});
+
 
 export default app;
