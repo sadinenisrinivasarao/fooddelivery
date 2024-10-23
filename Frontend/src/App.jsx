ab7c520792c24b97ea09ffc3048875478e8e7bf4
Srinivasa Rao Sadineni <sadinenisrinivasarao6@gmail.com>
2024-10-23 12:35:09 -0500

    lazyload update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,32 +1,35 @@
-import React, { useState } from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from './Pages/Home/Home'
-import Cart from './Pages/Cart/Cart'
-import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
-import StoreContextProvider from './Context/StoreContext'
-import Login from './Components/Login/Login'
+import React, { useState, lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import StoreContextProvider from './Context/StoreContext';
+
+const Navbar = lazy(() => import('./Components/Navbar/Navbar'));
+const Home = lazy(() => import('./Pages/Home/Home'));
+const Cart = lazy(() => import('./Pages/Cart/Cart'));
+const PlaceOrder = lazy(() => import('./Pages/PlaceOrder/PlaceOrder'));
+const Login = lazy(() => import('./Components/Login/Login'));
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
-  
+
   return (
     <div className='app'>
       <BrowserRouter>
-        <StoreContextProvider>
-          {showLogin ? <Login setShowLogin={setShowLogin} /> : <></>}
-          <Navbar setShowLogin={setShowLogin} />
-          
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/order" element={<PlaceOrder />} />
-          </Routes>
-        </StoreContextProvider>
+        <Suspense fallback={<h2>Loading...</h2>}>
+          <StoreContextProvider>
+            {showLogin && <Login setShowLogin={setShowLogin} />}
+            <Navbar setShowLogin={setShowLogin} />
+            
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/order" element={<PlaceOrder />} />
+            </Routes>
+          </StoreContextProvider>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
